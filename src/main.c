1b43707c4c84fffc97ce23fc92bc0b88cb0a048b
Tirth Patel <tirthpatel61@outlook.com>
2024-11-04 09:42:40 +0530

    Write a function to convert string to u32
    Define a function named `str_to_u32` in the 'main.c'. The function takes
    a null terminated string as a parameter and converts it to an unsigned
    32 bit integer. In case of invalid format, it returns U32_MAX to
    indicate error.
    
    Using U32_MAX as error code wouldn't be a problem because our program
    doesn't need to deal with such a large number.
    
    Instead of using `scanf()` to take the integer from the input, take the
    whole line from input and convert it to u32 using this function. This
    makes sure that user can only input one number in one line.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,22 @@
-#include <stdio.h>
 #include "mydefs.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+static u32 str_to_u32(const char *string) {
+  const size_t len = strlen(string);
+  u32 num = 0;
+
+  for (size_t i = 0; i < len; ++i) {
+    const char current_char = string[i];
+    if (current_char - '0' < 0 || current_char - '0' > 9)
+      return U32_MAX;  // Indicates invalid format
+
+    const u32 digit = current_char - '0';
+    num = num * 10 + digit;
+  }
+  return num;
+}
 
 int main() {
   printf("Guess the Number!\n");
